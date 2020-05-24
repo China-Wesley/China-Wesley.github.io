bf2f1534bc8273cc90c86c304ed2225c6bd70789
xin <1403576702@qq.com>
2020-05-24 09:45:54 +0800

    updata

--- WeyUI/src/App.vue
+++ WeyUI/src/App.vue
@@ -3,50 +3,52 @@
     <el-icon name="eleme"></el-icon>
     <el-icon name="loading"></el-icon>
     <i class="el-icon-edit"></i>
+    <el-row>
+      <!-- <el-col :span="10">qwe</el-col> -->
+    </el-row>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  },
+  name: "app",
+  components: {}
 };
 </script>
 
 <style>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
 
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
 
-  body > .el-container {
-    margin-bottom: 40px;
-  }
+body > .el-container {
+  margin-bottom: 40px;
+}
 
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
 
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
